024cfecde43724052eea039de9424c4bf21f6b95
KalpeshVadher <vadherkalpesh143@gmail.com>
2026-01-18 19:29:04 +0530

    Update logs.tf

--- terraform/logs.tf
+++ terraform/logs.tf
@@ -1,4 +1,9 @@
 resource "aws_cloudwatch_log_group" "ecs" {
   name              = "/ecs/simple-backend"
   retention_in_days = 7
+
+  lifecycle {
+    prevent_destroy = false      # safe destroy
+    ignore_changes  = [name]     # agar log group already exist, ignore
+  }
 }
