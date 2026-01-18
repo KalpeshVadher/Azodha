1bf3a3aa955650f041aae9073066ab4bed9a8a4b
KalpeshVadher <vadherkalpesh143@gmail.com>
2026-01-18 19:31:32 +0530

    Update ecs.tf

--- terraform/ecs.tf
+++ terraform/ecs.tf
@@ -14,9 +14,9 @@ resource "aws_security_group" "ecs" {
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
