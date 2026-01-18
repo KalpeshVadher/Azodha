e3e7751d1bdee9757b45c3931e854e9ec9e95e0b
KalpeshVadher <vadherkalpesh143@gmail.com>
2026-01-18 19:43:56 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,22 @@
 variable "aws_region" {
-  default = "ap-south-1"
+  description = "AWS region jahan resources deploy honge"
+  type        = string
+  default     = "ap-south-1"
 }
 
 variable "docker_image" {
-  description = "Docker Hub image"
+  description = "Docker Hub image for ECS container"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_count" {
+  description = "Number of public subnets"
+  type        = number
+  default     = 2
 }
