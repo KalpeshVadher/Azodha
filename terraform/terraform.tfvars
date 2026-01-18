4b66d1d008e605a8b521acb6db4af678c51c4fe1
KalpeshVadher <vadherkalpesh143@gmail.com>
2026-01-18 18:10:51 +0530

    Add files via upload

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,1 @@
+docker_image = "dockerhub_username/simplebackend:latest"
---
+++ terraform/variables.tf
@@ -0,0 +1,7 @@
+variable "aws_region" {
+  default = "ap-south-1"
+}
+
+variable "docker_image" {
+  description = "Docker Hub image"
+}
